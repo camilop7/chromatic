0065c75097bed5f7446d1e4e8c0b30f71bb85104
Jose Manuel Hita Garcia <josemanuelhita992@hotmail.com>
2023-03-07 10:02:02 +0000

    chatroom controller checkpoint

--- config/routes.rb
+++ config/routes.rb
@@ -3,9 +3,6 @@ Rails.application.routes.draw do
   root to: "pages#home"
   get "my_events", to: "pages#my_events"
 
-  resources :events do
-    resources :events_chatrooms, only: [:show] do
-      resources :messages, only: [:index, :new, :create, :destroy]
-    end
-  end
+  resources :events
+  resources :chatrooms, only: :show
 end
