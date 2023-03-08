a2cbde63b8b397449a5437919f45cc4ccc38ae0b
Camilo Palacios <camilop7@icloud.com>
2023-03-08 12:14:46 +0000

    claodinarysetup

--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,5 @@
 class Event < ApplicationRecord
   has_many :events_chatrooms
   has_many :users, through: :events_chatrooms
+  has_one_attached :photo
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -9,12 +9,15 @@
 puts "Cleaning database..."
 Event.destroy_all
 
-puts "Creating restaurants..."
-lady_gaga = {name: "Lady Gaga", address: "7 Boundary St, London E2 7JE"}
-eminem =  {name: "Eminem", address: "56A Shoreditch High St, London E1 6PQ"}
+# puts "Creating events..."
+# lady_gaga = {name: "Lady Gaga", address: "7 Boundary St, London E2 7JE"}
+# eminem =  {name: "Eminem", address: "56A Shoreditch High St, London E1 6PQ"}
 
-[lady_gaga, eminem].each do |attributes|
-  event = Event.create!(attributes)
-  puts "Created #{event.name}"
-end
-puts "Finished!"
+# [lady_gaga, eminem].each do |attributes|
+#   event = Event.create!(attributes)
+#   file = URI.open("http://t3.gstatic.com/licensed-image?q=tbn:ANd9GcSJF0amVXGcXz5A6Tkm40vozAeb9pEsCh4imG1wWBJxPy_OacG_o_YY19Ko7S8WksJk5cSUbf7JoourgT4")
+#   event.photo.attach(io: file, filename: "nes.png", content_type: "image/png")
+#   event.save
+#   puts "Created #{event.name}"
+# end
+# puts "Finished!"
