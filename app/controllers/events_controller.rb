bc103e0cfa5dd6153dcbfff9ea0e2484f0c2a146
Alex Delasalle <alex_delasalle@aol.co.uk>
2023-03-08 16:27:07 +0000

    Seeded and styled the event show page

--- app/controllers/events_controller.rb
+++ app/controllers/events_controller.rb
@@ -7,6 +7,11 @@ class EventsController < ApplicationController
 
   def show
     @event = Event.find(params[:id])
+
+    @markers = [{
+        lat: @event.latitude,
+        lng: @event.longitude
+      }]
   end
 
   def new
--- app/models/event.rb
+++ app/models/event.rb
@@ -1,4 +1,6 @@
 class Event < ApplicationRecord
   has_many :events_chatrooms
   has_many :users, through: :events_chatrooms
+  geocoded_by :address
+  after_validation :geocode, if: :will_save_change_to_address?
 end
