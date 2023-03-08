7e203e77868f139908e46a3e08ea732c3ec053fd
Emilie Clouet <emi.clouet@hotmail.fr>
2023-03-08 16:47:33 +0000

    added swiper and genre and styling

--- app/javascript/controllers/index.js
+++ app/javascript/controllers/index.js
@@ -6,3 +6,6 @@ import { application } from "./application"
 
 import HelloController from "./hello_controller"
 application.register("hello", HelloController)
+
+import SwiperController from "./swiper_controller"
+application.register("swiper", SwiperController)
