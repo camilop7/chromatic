fc631696bb7c70310fedc7fa0d2bdf23c7a6a388
Camilo Palacios <camilop7@icloud.com>
2023-03-08 00:08:09 +0000

    navbarfunctionalities

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,16 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+
+puts "Cleaning database..."
+Event.destroy_all
+
+puts "Creating restaurants..."
+lady_gaga = {name: "Lady Gaga", address: "7 Boundary St, London E2 7JE"}
+eminem =  {name: "Eminem", address: "56A Shoreditch High St, London E1 6PQ"}
+
+[lady_gaga, eminem].each do |attributes|
+  event = Event.create!(attributes)
+  puts "Created #{event.name}"
+end
+puts "Finished!"
