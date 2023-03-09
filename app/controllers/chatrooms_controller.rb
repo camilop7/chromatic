b8c7751a1db9c895ef191e3fbf3ab2049f572858
Jose Manuel Hita Garcia <josemanuelhita992@hotmail.com>
2023-03-09 16:31:31 +0000

    be ready for conflicts

--- app/controllers/chatrooms_controller.rb
+++ app/controllers/chatrooms_controller.rb
@@ -1,6 +1,7 @@
 class ChatroomsController < ApplicationController
   def show
     @chatroom = Chatroom.find(params[:id])
+    @event = Event.find(params[:event_id])
     @message = Message.new
   end
 end
