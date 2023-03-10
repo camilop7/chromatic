10797cde338a694b910eaa0dd65f1c1e133af2ab
Emilie Clouet <emi.clouet@hotmail.fr>
2023-03-10 13:25:11 +0000

    create new table user_event

--- db/schema.rb
+++ db/schema.rb
@@ -10,7 +10,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema[7.0].define(version: 2023_03_09_122037) do
+ActiveRecord::Schema[7.0].define(version: 2023_03_10_131031) do
   # These are extensions that must be enabled in order to support this database
   enable_extension "plpgsql"
 
@@ -44,6 +44,15 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_122037) do
     t.index ["user_id"], name: "index_messages_on_user_id"
   end
 
+  create_table "user_events", force: :cascade do |t|
+    t.bigint "user_id", null: false
+    t.bigint "event_id", null: false
+    t.datetime "created_at", null: false
+    t.datetime "updated_at", null: false
+    t.index ["event_id"], name: "index_user_events_on_event_id"
+    t.index ["user_id"], name: "index_user_events_on_user_id"
+  end
+
   create_table "users", force: :cascade do |t|
     t.string "email", default: "", null: false
     t.string "encrypted_password", default: "", null: false
@@ -59,4 +68,6 @@ ActiveRecord::Schema[7.0].define(version: 2023_03_09_122037) do
   add_foreign_key "chatrooms", "events"
   add_foreign_key "messages", "chatrooms"
   add_foreign_key "messages", "users"
+  add_foreign_key "user_events", "events"
+  add_foreign_key "user_events", "users"
 end
