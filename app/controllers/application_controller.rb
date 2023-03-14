1124a296505c2e701a6d75fc9f6e3df2e37f6c74
Alex Delasalle <alex_delasalle@aol.co.uk>
2023-03-14 12:54:31 +0000

    allowed the new sign up names to picked up in the chatroom

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,7 +1,15 @@
 class ApplicationController < ActionController::Base
   before_action :authenticate_user!
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   def user_params
-    params.require(:user).permit(:photo)
+    params.require(:user).permit(:name, :photo)
+  end
+
+  protected
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up, keys: [:name])
+    devise_parameter_sanitizer.permit(:account_update, keys: [:name])
   end
 end
