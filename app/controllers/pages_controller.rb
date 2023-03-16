d57194adc2f330a25b658e3deac1b6961374baf6
Jose Manuel Hita Garcia <josemanuelhita992@hotmail.com>
2023-03-16 14:08:16 +0000

    created a videocall

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -11,4 +11,8 @@ class PagesController < ApplicationController
     @user_events = UserEvent.where(user: current_user)
     @friendships = Friendship.where(user: current_user).or(Friendship.where(friend: current_user))
   end
+
+  def videocall
+    @chatroom = Chatroom.where(params[:chatroom_id])
+  end
 end
